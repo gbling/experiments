cb365a5fe06bf141d35d58409764f88c62614290
gbling <7369074+gbling@user.noreply.gitee.com>
2024-03-23 12:03:35 +0800

    添加go语言支持

---
+++ ebpf/go/helloworld/helloworld.bpf.c
@@ -0,0 +1,10 @@
+#include "common.h"
+
+SEC("tracepoint/syscalls/sys_enter_execve")
+int bpf_prog(void *ctx) {
+	char msg[] = "hello world!";
+	bpf_printk("invoke bpf_prog: %s\n", msg);
+	return 0;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
